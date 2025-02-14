a01ee174d838af17285312ea8771b433206a39d9
AYUSHI-TALEWAR-23 <121218383+AYUSHI-TALEWAR-23@users.noreply.github.com>
2025-02-14 14:38:33 +0530

    Added async/await fs File Creation

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function displayMessage() {
     })
 }
 displayMessage().then((message) => console.log(message));
+
+const fs = require('fs').promises;
+async function writeFileAsync(){
+    try {
+        await fs.writeFile('Hello.txt', 'Hi Nodejs File is created successfully.')
+        console.log('file created successfully')
+    } catch (error) {
+        console.error('error while creating file', error)        
+    }
+}
+writeFileAsync();
